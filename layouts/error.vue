f4c810f7922cb1b9488b1de9a288c870c4d67700
verdantpath <m_dittmann@yahoo.com>
2023-04-13 19:30:41 -0700

    Add error layout

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="text-align: center;">
+    <img src="https://media.tenor.com/ZRKlPRoS30cAAAAd/alf-thats-not-what-i-wrote.gif">
+    <h1>Error!</h1>
+    {{ error.message }} {{ error.statusCode }}
+  </div>
+</template>
+<script>
+  export default {
+    head() {
+      return {
+        title: 'Big problemo'
+      }
+    },
+    props: {
+      error: {
+        type: Object,
+        required: true, 
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
