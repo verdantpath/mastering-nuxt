ef9a7b827f87de38079e831b20c45268a802d694
verdantpath <m_dittmann@yahoo.com>
2023-08-20 14:34:08 -0700

    Update default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,60 @@
 <template>
-  <div>
-    <header style="background-color:#eee;">
-      <nuxt-link to="/">Home</nuxt-link>
-      <input type="text" ref="citySearch" @changed="changed" />
+  <div class="app">
+    <header class="app-header">
+      <div class="app-logo">
+        <img src="/images/logo.svg" />
+      </div>
+      <div class="app-search">
+        <input type="text" ref="citySearch" @changed="changed" placeholder="Enter your address" />
+        <input type="text" class="datepicker" placeholder="Check in">
+        <input type="text" class="datepicker" placeholder="Check out">
+        <button>
+          <img src="/images/icons/search.svg" />
+        </button>
+      </div>
+      <div class="app-user-menu">
+        <template v-if="isLoggedIn">
+          <img src="/images/icons/house.svg" />
+          <div class="name">Host</div>
+          <img :src="user.profileUrl" class="avatar" /> 
+        </template>
+        <div v-show="!isLoggedIn" id="googleButton" class="ml-8">
+          <div
+            id="g_id_onload"
+            :data-client_id="$config.auth.clientId"
+            data-auto_select="true"
+            data-callback="auth"
+          ></div>
+          <div
+            class="g_id_signin"
+            data-type="icon"
+            data-shape="circle"
+            data-theme="outline"
+            data-text="signin_with"
+            data-size="large"
+          ></div>
+        </div>
+      </div>
     </header>
     <nuxt />
   </div>
 </template>
 <script>
   export default {
+    created() {
+      // console.log('created:', this.$config.test1, this.$config.test2)
+    },
     mounted() {
       this.$maps.makeAutoComplete(this.$refs.citySearch);
+    },
+    computed: {
+      user() {
+        return this.$store.state.auth.user
+      },
+      isLoggedIn() {
+        return this.$store.state.auth.isLoggedIn
+        // console.log('hello')
+      },
     },
     methods: {
       changed(event) {
