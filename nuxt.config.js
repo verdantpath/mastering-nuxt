7a3eb3b0bce9518ed8a1a0e1190bdec06f8c35df
verdantpath <m_dittmann@yahoo.com>
2023-08-24 20:08:28 -0700

    Update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,11 +15,42 @@ export default {
   router: {
     prefetchLinks: false
   },
-  plugins: ['~/plugins/maps.client',
+  plugins: [
+    '~/plugins/maps.client',
     '~/plugins/dataApi',
+    '~/plugins/auth.client',
   ],
   modules: [
+    '~/modules/auth',
+    '~/modules/algolia',
   ],
-  buildModules: ['@nuxtjs/tailwindcss']
+  buildModules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  css: [
+    '~assets/sass/app.scss'
+  ],
+  build: {
+    extractCSS: true,
+    loaders: {
+      limit: 0, 
+    }
+  },
+  publicRuntimeConfig: {
+    auth: {
+      cookieName: 'idToken',
+      clientId: '209367763963-skp5qcc6n54in1tgfnnia8evtb6rj42q.apps.googleusercontent.com',
+    },
+    algolia: {
+      appId: 'DSC80G85H6',
+      key: '4b03de1a0fb2f091ee9a8212600a214c',
+    }
+  },
+  privateRuntimeConfig: {
+    algolia: {
+      appId: 'DSC80G85H6',
+      key: '9cf34ba988f71ae3a67909afe36f2935',
+    }
+  },
 
 }
