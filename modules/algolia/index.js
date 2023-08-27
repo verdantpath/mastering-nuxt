1e40c3243575fe081a5bd63a3c2000f36312936c
verdantpath <m_dittmann@yahoo.com>
2023-08-27 11:52:18 -0700

    Add algolia module

---
+++ modules/algolia/index.js
@@ -0,0 +1,56 @@
+import fetch from 'node-fetch'
+import { unWrap, getErrorResponse } from '../../utils/fetchUtils'
+import { getHeaders, sendJSON } from './helpers'
+
+export default function() {
+  const algoliaConfig = this.options.privateRuntimeConfig.algolia
+  const headers = getHeaders(algoliaConfig)
+
+  this.nuxt.hook('render:setupMiddleware', (app) => {
+    app.use('/api/user', getUserRoute)
+  })
+
+  async function getUserRoute(req, res, next) {
+    const identity = req.identity
+    const userData = await getUserById(identity)
+    if(userData.status == 200) {
+      sendJSON(userData.json, res)
+      return
+    }
+    createUser(req.identity)
+    sendJSON(makeUserPayload(identity), res)
+    next()
+  }
+  async function createUser(identity) {
+    try {
+      return unWrap(await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${identity.id}`, {
+          headers,
+          method: 'PUT',
+          body: JSON.stringify(makeUserPayload(identity))
+      }))
+    } catch(error){
+        return getErrorResponse(error)
+    }
+  }
+  async function getUserById(identity) {
+    try {
+      return unWrap(await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${identity.id}`, {
+          headers,
+      }))
+    } catch(error){
+        return getErrorResponse(error)
+    }
+  }
+
+  function makeUserPayload(identity) {
+    return {
+      name: identity.name,
+      email:identity.email,
+      image: identity.image,
+      homeId: [],
+      reviewCount: 0,
+      description: '',
+      joined: new Date().toISOString()
+    }
+  }
+}
