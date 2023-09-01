ea01251463827d0a19bd7e3f429cc71691bda4ea
verdantpath <m_dittmann@yahoo.com>
2023-08-31 20:29:02 -0700

    Update auth plugin

---
+++ plugins/auth.client.js
@@ -0,0 +1,38 @@
+import Cookie from "js-cookie";
+import jwt_decode from "jwt-decode";
+
+export default ({ $config, store }, inject) => {
+    window.auth = (response) => {
+        try {
+            const token = response.credential;
+            const user = jwt_decode(token);
+            store.commit("auth/user", {
+                fullName: user.name,
+                profileUrl: user.picture,
+            });
+
+            Cookie.set($config.auth.cookieName, token, {
+                expires: 1 / 24,
+                sameSite: "Lax",
+            });
+        } catch (e) {
+            console.log(`JWT Error ${e}`);
+        }
+    };
+    addScript();
+    inject("auth", {
+        signOut,
+    });
+
+    function addScript() {
+        const script = document.createElement("script");
+        script.src = "https://accounts.google.com/gsi/client";
+        script.async = true;
+        document.head.appendChild(script);
+    }
+
+    function signOut() {
+        Cookie.remove($config.auth.cookieName);
+        store.commit("auth/user", null);
+    }
+};
