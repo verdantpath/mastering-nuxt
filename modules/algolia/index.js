7be9944bf31cf0c7ddac6163cf37b9403eb1d94f
verdantpath <m_dittmann@yahoo.com>
2023-09-05 20:28:49 -0700

    Add algolia module

--- modules/algolia/index.js
+++ modules/algolia/index.js
@@ -1,56 +1,15 @@
-import fetch from 'node-fetch'
-import { unWrap, getErrorResponse } from '../../utils/fetchUtils'
-import { getHeaders, sendJSON } from './helpers'
+import userRouter from './routers/user'
+import getApis from './apis'
+import bodyParser from 'body-parser'
 
 export default function() {
   const algoliaConfig = this.options.privateRuntimeConfig.algolia
-  const headers = getHeaders(algoliaConfig)
-
+  const apis = getApis(algoliaConfig)
+   
   this.nuxt.hook('render:setupMiddleware', (app) => {
-    app.use('/api/user', getUserRoute)
+    app.use(bodyParser.urlencoded())
+    app.use('/api/user', userRouter(apis))
   })
 
-  async function getUserRoute(req, res, next) {
-    const identity = req.identity
-    const userData = await getUserById(identity)
-    if(userData.status == 200) {
-      sendJSON(userData.json, res)
-      return
-    }
-    createUser(req.identity)
-    sendJSON(makeUserPayload(identity), res)
-    next()
-  }
-  async function createUser(identity) {
-    try {
-      return unWrap(await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${identity.id}`, {
-          headers,
-          method: 'PUT',
-          body: JSON.stringify(makeUserPayload(identity))
-      }))
-    } catch(error){
-        return getErrorResponse(error)
-    }
-  }
-  async function getUserById(identity) {
-    try {
-      return unWrap(await fetch(`https://${algoliaConfig.appId}-dsn.algolia.net/1/indexes/users/${identity.id}`, {
-          headers,
-      }))
-    } catch(error){
-        return getErrorResponse(error)
-    }
-  }
-
-  function makeUserPayload(identity) {
-    return {
-      name: identity.name,
-      email:identity.email,
-      image: identity.image,
-      homeId: [],
-      reviewCount: 0,
-      description: '',
-      joined: new Date().toISOString()
-    }
-  }
+  
 }
